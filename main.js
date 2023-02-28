4de63835a96a649461f59f87075b38e689734ef0
Andy <andythomas.py@gmail.com>
2023-02-28 13:23:14 -0600

    Added event listeneres

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
-// Manipulating the DOM
-const ul = document.querySelector('.items');
-
-// ul.remove();
-// ul.lastElementChild.remove();
-ul.firstElementChild.textContent = 'Hello';
-ul.children[1].innerText = 'Brad';
-ul.lastElementChild.innerHTML = '<h1>Hello</h1>';
-
 const btn = document.querySelector('.btn');
-btn.style.background = 'red';
+
+// addEventListener takes two inputs...the event and the function you want to run when the button is clicked
+
+// btn.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     document.querySelector('#my-form').style.background = '#ccc';
+//     document.querySelector('body').classList.add('bg-dark');
+//     document.querySelector('.items').lastElementChild.innerHTML = '<h1>Hello</h1>';
+//     document.querySelector('.items').lastElementChild.style.color = 'black';
+// });
+
+// btn.addEventListener('mouseover', (e) => {
+//     e.preventDefault();
+//     document.querySelector('#my-form').style.background = '#ccc';
+//     document.querySelector('body').classList.add('bg-dark');
+//     document.querySelector('.items').lastElementChild.innerHTML = '<h1>Hello</h1>';
+//     document.querySelector('.items').lastElementChild.style.color = 'black';
+// });
+
+
+btn.addEventListener('mouseout', (e) => {
+    e.preventDefault();
+    document.querySelector('#my-form').style.background = '#ccc';
+    document.querySelector('body').classList.add('bg-dark');
+    document.querySelector('.items').lastElementChild.innerHTML = '<h1>Hello</h1>';
+    document.querySelector('.items').lastElementChild.style.color = 'black';
+});
