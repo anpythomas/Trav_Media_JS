25fcfe50ce8cfa3a97006f4272ee5dc6ac35b46f
Andy <andythomas.py@gmail.com>
2023-02-28 12:44:28 -0600

    Added mulitple ways to get items

--- main.js
+++ main.js
@@ -1,40 +1,14 @@
-// Constructor Function
+// Single Element
+const form = document.getElementById('my-form');
+console.log(form);
 
-// function Person(firstName, lastName, dob) {
-//     this.firstName = firstName;
-//     this.lastName = lastName;
-//     this.dob = new Date(dob);
-//     this.getBirthYear = function() {
-//         return this.dob.getFullYear();
-//     }
-//     this.getFullName = function() {
-//         return `${this.firstName} ${this.lastName}`
-//     }
-// }
+console.log(document.querySelector('h1'));
 
-//Class with methods
-class Person {
-    constructor(firstName, lastName, dob) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.dob = new Date(dob);
-    }
+// Multiple Elements
 
-    getBirthYear() {
-        return this.dob.getFullYear();
-    }
+// Newest and preffered method
+console.log(document.querySelectorAll('.item'));
 
-    getFullName() {
-        return `${this.firstName} ${this.lastName}`;
-    }
-}
-
-
-// Instantiate object
-const person1 = new Person('John', 'Doe', '2-3-1983');
-const person2 = new Person('Mary', 'Smith', '9-3-1986');
-
-console.log(person2.getBirthYear());
-console.log(person2.getFullName());
-
-console.log(person1);
+// These two are older less efficient ways
+console.log(document.getElementsByClassName('item'));
+console.log(document.getElementsByTagName('li'));
